f76798b2e4deeedac068ba036d2ee8e9a31d5cfc
AbhijithTyss <abhijith.styss@gmail.com>
2024-07-16 09:41:00 +0530

    Poll scm class added

---
+++ SeleniumJenkins830/src/test/java/jenkins/PollScmTest.java
@@ -0,0 +1,13 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class PollScmTest {
+
+	@Test
+	public void poll() {
+		Reporter.log("Poll scm class added",true);
+	}
+
+}
